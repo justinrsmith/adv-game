a7ae35df2bf5e4a9b22059e745d08672615ceef5
Justin Smith <justinsmith@Justins-MacBook-Pro.local>
2013-04-06 11:51:13 -0500

    first

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+	
+config = {
+	'description': 'My Project',
+	'author': 'Justin Smith',
+	'url', 'URL to get it at.',
+	'download_url': 'Where to download it.',
+	'authoer_email': 'justinrsmith88@gmail.com',
+	'version': '0.1',
+	'install_requires': ['nose'],
+	'packages': ['adv'],
+	'scripts': [],
+	'name': 'projectname'
+}
+
+setup(**config)
---
+++ tests/adv_tests.py
@@ -0,0 +1,54 @@
+from nose.tools import *
+from adv.game import BuildHero
+from adv.game import Engine
+from adv.game import Outskirts
+from adv.game import Depart
+from adv.game import Shop
+from adv.game import Rest
+from adv.game import Outskirts
+from adv.game import Downtown
+from adv.game import Tunnels
+from adv.game import Subway
+from adv.game import Battle
+
+def test_BuildHero():
+	hero = BuildHero('Paperboy')
+	hero.printStats()
+	
+def test_Engine():
+	engine = Engine(1,'Paperboy')
+	engine.Run(1,'Paperboy')
+	
+def test_Depart():
+	depart = Depart('Paperboy')
+	depart.Action()
+	
+def test_Shop():
+	shop = Shop('Paperboy', 'Subway')
+	shop.Action()
+	
+def rest_Test():
+	rest = Rest('Paperboy', 'Subway')
+	rest.Action()
+	
+def test_Outskirts():
+	outskirts = Outskirts('Paperboy')
+	outskirts.Action()
+	
+def test_Downtown():
+	downtown = Downtown('Paperboy','Outskirts')
+	downtown.Action()
+	
+def test_Subway():
+	subway = Subway('Paperboy','Downtown')
+	subway.Action()
+	
+#def test_Tunnels():
+#	tunnels = Tunnels('Paperboy','Subway')
+#	tunnels.Action()
+	
+#def test_Battle():
+	#battle = Battle('Downtown')
+	#battle.Action()
+	#battle.Attack(200,150,75,50,'Crony')
+	#battle.Defend(200,150,75,50,'Crony')
